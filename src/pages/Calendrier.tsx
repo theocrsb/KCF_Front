d9fa9bd7fbf32f2985b3bd1e7f53026236eb657c
Theocrsb <theo_crosbie@yahoo.fr>
2023-01-18 18:01:33 +0100

    test en cours ajout des cours par defaut

--- src/pages/Calendrier.tsx
+++ src/pages/Calendrier.tsx
@@ -7,25 +7,115 @@ import dayjs from 'dayjs';
 import 'dayjs/locale/fr';
 import locale from 'antd/locale/fr_FR';
 import Badge, { BadgeProps } from 'antd/es/badge';
+import { Dayjs } from 'dayjs';
+
+export interface Cours {
+  id: string;
+  sensei: string;
+  date: string;
+  heureDebut: string;
+  heureFin: string;
+  note: string;
+  // user: User
+}
 
 const Calendrier = () => {
-  const [date, setDate] = useState(new Date());
+  const [dateSelect, setDateSelect] = useState<string>('');
 
-  const myLocal = {
-    ...frFR,
-    dateFormat: 'DD/MM/YYYY',
+  //Lors d'un click sur une date du Calendar
+  const handleSelect = (select: Dayjs) => {
+    console.log(select.format('DD-MM-YYYY'));
+    setDateSelect(select.format('DD-MM-YYYY'));
   };
-  //Trouver comment gerer le jour pour l'affichage
-  const dateCellRender = () => {
-    const valueCalendar = {
-      date: '18-01-2023',
-      type: 'warning',
-      content: 'This is warning event.',
-    };
-    return (
-      <Badge status={'success' as BadgeProps['status']} text={'cours kata'} />
-    );
+
+  // affichage ou nom d'un event selon la date
+  // const getListData = (value: Dayjs) => {
+  //   //ajout date en brut (futur bdd)
+  //   const dateBdd = new Date('2023-01-21');
+
+  //   let listData;
+  //   switch (value.daysInMonth()) {
+  //     case 2023 - 0 - 21:
+  //       listData = [
+  //         { type: 'warning', content: 'This is warning event.' },
+  //         { type: 'success', content: 'This is usual event.' },
+  //       ];
+  //       break;
+  //     // case 10:
+  //     //   listData = [
+  //     //     { type: 'warning', content: 'This is warning event.' },
+  //     //     { type: 'success', content: 'This is usual event.' },
+  //     //     { type: 'error', content: 'This is error event.' },
+  //     //   ];
+  //     //   break;
+  //     // case 15:
+  //     //   listData = [
+  //     //     { type: 'warning', content: 'This is warning event' },
+  //     //     { type: 'success', content: 'This is very long usual event。。....' },
+  //     //     { type: 'error', content: 'This is error event 1.' },
+  //     //     { type: 'error', content: 'This is error event 2.' },
+  //     //     { type: 'error', content: 'This is error event 3.' },
+  //     //     { type: 'error', content: 'This is error event 4.' },
+  //     //   ];
+  //     //   break;
+  //     // default:
+  //   }
+  //   return listData || [];
+  // };
+  // //
+
+  // //Affichage des cours dans le Calendar
+  // const dateCellRender = (value: Dayjs) => {
+  //   //Exemple lors d'un get d'un cours
+  //   // const valueCalendar: Cours[] = [
+  //   //   {
+  //   //     id: '01234',
+  //   //     date: '19-01-2023',
+  //   //     sensei: 'string',
+  //   //     heureDebut: 'string',
+  //   //     heureFin: 'string',
+  //   //     note: 'string',
+  //   //     // type: 'warning',
+  //   //     // content: 'BABY KARATE',
+  //   //   },
+  //   // ];
+
+  //   const listData = getListData(value);
+
+  // return (
+  //   <ul className='events'>
+  //     {listData.map((item) => (
+  //       <li key={item.content}>
+  //         <Badge
+  //           status={item.type as BadgeProps['status']}
+  //           text={item.content}
+  //         />
+  //       </li>
+  //     ))}
+  //   </ul>
+  // );
+
+  const events = [
+    { date: '2023-01-01', name: 'Karaté kata' },
+    { date: '2023-01-03', name: 'Karaté combat' },
+    { date: '2023-01-05', name: 'Karaté BABY' },
+  ];
+
+  const dateFullCellRender = (value: Dayjs) => {
+    const date = value.format('YYYY-MM-DD');
+    const event = events.find((e) => e.date === date);
+    if (event) {
+      return (
+        <div>
+          {value.date()}
+          <div>{event.name}</div>
+        </div>
+      );
+    } else {
+      return <div>{value.date()}</div>;
+    }
   };
+
   return (
     <div
       className='d-flex justify-content-center '
@@ -36,7 +126,8 @@ const Calendrier = () => {
           {/* dateCellRender : Remplis une valeurs dans le jours */}
           <Calendar
             style={{ padding: '7.5%', borderRadius: '15px' }}
-            dateCellRender={dateCellRender}
+            dateFullCellRender={dateFullCellRender}
+            onSelect={handleSelect}
             defaultValue={dayjs('17-01-2023', 'DD-MM-YYYY')}
           />
         </ConfigProvider>
