873dc8a2ba133fdc38e52b9d1ab96aef1fcd5de8
Theocrsb <theo_crosbie@yahoo.fr>
2023-02-15 07:58:02 +0100

    switch axios

--- src/axios/instance-axios.ts
+++ src/axios/instance-axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const instanceAxios = axios.create({
-  baseURL: 'http://localhost:8080/api/',
-  // baseURL: 'https://karateclubfosses.onrender.com/api/',
+  // baseURL: 'http://localhost:8080/api/',
+  baseURL: 'https://karateclubfosses.onrender.com/api/',
   headers: {
     'Content-Type': 'application/json',
   },
