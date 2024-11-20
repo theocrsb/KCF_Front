a43b20aab8b653110b2911a2b62400e3542e5cc4
theocrsb <theo_crosbie@yahoo.fr>
2024-11-20 21:13:44 +0100

    update navbar style
    Signed-off-by: theocrsb <theo_crosbie@yahoo.fr>

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -17,20 +17,6 @@ const NavBar = () => {
             | Karaté Club Fosses
           </NavLink>
         </div>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
-        </div>
       </div>
     </nav>
   );
